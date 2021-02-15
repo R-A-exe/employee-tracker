f9292e15e2a6c5d0be5d2dcc717f332d1de5a284
R-A-exe <royatalla.ra@gmail.com>
2021-02-15 13:46:47 -0500

    Added viewBy method to view employees by role, manager or department

--- db/db.js
+++ db/db.js
@@ -20,9 +20,10 @@ async function view(table) {
         case 'role':
             query = 'SELECT role.id, role.title, role.salary, department.name FROM role JOIN department ON department_id = department.id;'
             break;
-            
+
         case 'employee':
-            query = 'SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id'
+            query = 'SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name, employee2.first_name AS manager_first_name, employee2.last_name AS manager_last_name FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id LEFT JOIN employee as employee2 ON employee.manager_id = employee2.id';
+            break;
     }
     return new Promise(async function (resolve, reject) {
         connection.query(query, (err, res) => {
@@ -30,6 +31,25 @@ async function view(table) {
             return (resolve(res));
         });
     });
+}
+
+async function viewBy(field, obj) {
+    switch(field){
+        case 'manager':
+            field = 'employee.manager_id';
+            break;
+        case 'role':
+            field = 'employee.role_id';
+            break;
+        case 'department':
+            field = 'role.department_id';
+    }
+    return new Promise(async function(resolve, reject){
+        connection.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name, employee2.first_name AS manager_first_name, employee2.last_name AS manager_last_name FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id LEFT JOIN employee as employee2 ON employee.manager_id = employee2.id WHERE ?? = ?',[field, obj.id], (err, res)=>{
+            if(err) return (reject(err));
+            return (resolve(res))
+        });
+    })
 }
 
 async function add(table, obj) {
@@ -45,7 +65,7 @@ async function updateEmployee(employee, field) {
     return new Promise(async function (resolve, reject) {
         connection.query('UPDATE employee SET ?? = ? WHERE id = ?', [field, employee[field], employee.id], (err, res) => {
             if (err) return (reject(err));
-            return (resolve(res.up));
+            return (resolve(res.affectedRows));
         })
     });
 
@@ -56,5 +76,6 @@ async function updateEmployee(employee, field) {
 module.exports = {
     view,
     add,
-    updateEmployee
+    updateEmployee,
+    viewBy
 };
