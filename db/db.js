d566083e7ff6d1065671f04a759d519894a20c2c
R-A-exe <royatalla.ra@gmail.com>
2021-02-15 12:47:16 -0500

    Added updateEmployee function update fields for employee record

--- db/db.js
+++ db/db.js
@@ -21,17 +21,26 @@ async function view(table){
 
 async function add(table, obj){
     return new Promise (async function(resolve, reject){
-        var q = connection.query('INSERT INTO ?? VALUES(default, ?)', [table, obj],(err, res)=>{
-            
+        connection.query('INSERT INTO ?? VALUES(default, ?)', [table, obj],(err, res)=>{    
             if(err) return(reject(err));
             return(resolve(res.insertId));
         });
-        console.log(q);
     });
+}
+
+async function updateEmployee(employee, field){
+    return new Promise (async function(resolve, reject){
+        connection.query('UPDATE employee SET ?? = ? WHERE id = ?',[field, employee[field], employee.id], (err,res)=>{
+            if(err) return(reject(err));
+            return(resolve(res.up));
+        })
+    });
+
 }
 
 
 module.exports = {
     view,
-    add
+    add,
+    updateEmployee
 };
