79e4128761eedb6576927adeab09496d4e8c0856
R-A-exe <royatalla.ra@gmail.com>
2021-02-15 12:55:54 -0500

    Updated view function to add details to each view through joins

--- db/db.js
+++ db/db.js
@@ -1,8 +1,8 @@
-const mysql = require ('mysql');
+const mysql = require('mysql');
 
 
 var connection = mysql.createConnection({
-    host:'localhost',
+    host: 'localhost',
     port: 3306,
     user: 'testUser',
     password: 'password',
@@ -10,33 +10,47 @@ var connection = mysql.createConnection({
 });
 
 
-async function view(table){
-    return new Promise (async function (resolve, reject){
-        connection.query('SELECT * FROM ??',table,(err, res)=>{
-            if (err) return(reject(err));
+async function view(table) {
+    var query = null;
+    switch (table) {
+        case 'department':
+            query = 'SELECT * FROM department';
+            break;
+
+        case 'role':
+            query = 'SELECT role.id, role.title, role.salary, department.name FROM role JOIN department ON department_id = department.id;'
+            break;
+            
+        case 'employee':
+            query = 'SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id'
+    }
+    return new Promise(async function (resolve, reject) {
+        connection.query(query, (err, res) => {
+            if (err) return (reject(err));
             return (resolve(res));
         });
     });
 }
 
-async function add(table, obj){
-    return new Promise (async function(resolve, reject){
-        connection.query('INSERT INTO ?? VALUES(default, ?)', [table, obj],(err, res)=>{    
-            if(err) return(reject(err));
-            return(resolve(res.insertId));
+async function add(table, obj) {
+    return new Promise(async function (resolve, reject) {
+        connection.query('INSERT INTO ?? VALUES(default, ?)', [table, obj], (err, res) => {
+            if (err) return (reject(err));
+            return (resolve(res.insertId));
         });
     });
 }
 
-async function updateEmployee(employee, field){
-    return new Promise (async function(resolve, reject){
-        connection.query('UPDATE employee SET ?? = ? WHERE id = ?',[field, employee[field], employee.id], (err,res)=>{
-            if(err) return(reject(err));
-            return(resolve(res.up));
+async function updateEmployee(employee, field) {
+    return new Promise(async function (resolve, reject) {
+        connection.query('UPDATE employee SET ?? = ? WHERE id = ?', [field, employee[field], employee.id], (err, res) => {
+            if (err) return (reject(err));
+            return (resolve(res.up));
         })
     });
 
 }
+
 
 
 module.exports = {
