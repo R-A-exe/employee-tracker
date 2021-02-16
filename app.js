d06e6e3bba8a331c4112f3d3dd59a4f84976c80d
R-A-exe <royatalla.ra@gmail.com>
2021-02-15 20:32:12 -0500

    Added Roles meny with functions to add and delete roles

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 const db = require('./db/db');
-const Employee = require('./classes/employee');
-const Role = require('./classes/role');
-const Department = require('./classes/department');
 const inquirer = require('inquirer');
-const cTable = require('console.table');
 
 
 
@@ -110,8 +106,8 @@ async function deleteDepartment(depts) {
         ]).then(async conf => {
             if (conf.confirm) {
                 try {
-                    await db.deleteRecord('department', depts.find(el => el.name == e.name));
-                    console.log(`${e.name} successfully deleted`);
+                    await db.deleteRecord('department', depts.find(el => el.name == del));
+                    console.log('Successfully deleted');
                 } catch (err) {
                     console.log(err);
                 }
@@ -141,7 +137,106 @@ async function viewDepartmentBudget(depts) {
 }
 
 async function rolesMenu() {
-    console.log('Roles menu')
+    var roles = await db.view('role');
+    console.table('Roles', roles);
+
+    await inquirer.prompt([
+        {
+            name: 'action',
+            type: 'list',
+            choices: ['Add role', 'Delete role', 'Back'],
+            message: 'What would you like to do?'
+        }
+    ]).then(async e => {
+        switch (e.action) {
+            case 'Add role':
+                addRole();
+                break;
+            case 'Delete role':
+                deleteRole(roles);
+                break;
+
+            case 'Back':
+                mainMenu();
+                break;
+        }
+    });
+}
+
+async function addRole() {
+    var depts = await db.view('department');
+    await inquirer.prompt([
+        {
+            name: 'title',
+            type: 'input',
+            message: 'Type the title of the role you would like to add',
+            validate: async (e) => {
+                return e.trim() == "" ? 'Please enter a valid name.' : true;
+            }
+        },
+        {
+            name: 'salary',
+            type: 'input',
+            message: 'Enter the salary for the role (e.g.50000).',
+            validate: async (e) => {
+                return /^\d+$/.test(e) ? true : 'Please enter a valid amount';
+            }
+        },
+        {
+            name: 'department',
+            type: 'list',
+            choices: depts.map(e => e.name),
+            message: 'Which department does this role belong to?'
+        },
+    ]).then(async res => {
+        try {
+            var newRole = [res.title, parseInt(res.salary), depts.find(el => el.name == res.department).id]
+            db.add('role', newRole);
+            console.log(`Successfully added ${res.title} role.`)
+        } catch (err) {
+            console.log(err);
+        }
+    });
+    rolesMenu();
+}
+
+async function deleteRole(roles){
+    var del = null
+    var rolesMap = new Map();
+    roles.forEach(element=>{
+        rolesMap.set(element.title, )
+    })
+    await inquirer.prompt([
+        {
+            name: 'id',
+            type: 'input',
+            message: 'Type the ID the role you would like to delete.',
+            validate: async(e)=>{
+                return /^\d+$/.test(e) ? true : 'Please enter a valid ID';
+            }
+        }
+    ]).then(async res=>{
+        var del = roles.find(e=>e.id==parseInt(res.id));
+        console.table(del);
+        await inquirer.prompt([
+            {
+                name: 'confirm',
+                type: 'confirm',
+                message: `Are you sure you want to delete this role?`,
+                default: false
+            }
+        ]).then(async conf=>{
+            if(conf.confirm){
+                try{
+                    await db.deleteRecord('role', del);
+                    console.log('Successfully deleted');
+                }catch(err){
+                    console.log(err);
+                }
+            }
+        });
+    });
+    rolesMenu();
 }
 
 async function employeesMenu() {
